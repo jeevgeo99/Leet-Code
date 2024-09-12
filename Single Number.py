a41fd0a26996bfc846065cb05163cafdca82723d
Jeevan George John <35341761+jeevgeo99@users.noreply.github.com>
2024-09-13 00:38:32 +0100

    Add files via upload

--- Single Number.py
+++ Single Number.py
@@ -1,14 +1,12 @@
-from typing import List
-class Soltion:
-    def singleNumber(self,nums:List[int]) ->int:
-
-        unique={}
-
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        unique = {}
+        
         for num in nums:
             if num in unique:
-                del unique[num]
+                del unique[num]  # Remove if the number appears twice
             else:
-                unique[num] = 1
+                unique[num] = 1  # Add to the dictionary if seen for the first time
         
-        return list(unique.keys()) [0]
-
+        # At the end, the dictionary will contain only one element, the single number
+        return list(unique.keys())[0]
